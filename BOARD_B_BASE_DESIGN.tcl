51f1ef337fcc1a082bef6ca830b0a2ef1e0b24dc
vauban353 <vauban353@gmail.com>
2021-10-31 15:04:26 +0000

    Renamed header pin constraints file.

--- BOARD_B_BASE_DESIGN.tcl
+++ BOARD_B_BASE_DESIGN.tcl
@@ -139,7 +139,7 @@ import_files \
 	-convert_EDN_to_HDL 0 \
 	-io_pdc "${constraint_path}/ICICLE.pdc" \
 	-io_pdc "${constraint_path}/ETHERNET_MAC.pdc" \
-	-io_pdc "${constraint_path}/ICICLE_MIKROBUS.pdc" \
+	-io_pdc "${constraint_path}/P9.pdc" \
 	-io_pdc "${constraint_path}/ICICLE_PCIE.pdc" \
 	-io_pdc "${constraint_path}/ICICLE_USB.pdc" \
 	-io_pdc "${constraint_path}/ICICLE_SDIO.pdc" \
@@ -159,7 +159,7 @@ import_files -convert_EDN_to_HDL 0 -fp_pdc "${constraint_path}/CCC.pdc"
 
 organize_tool_files \
 	-tool {PLACEROUTE} \
-	-file "${project_dir}/constraint/io/ICICLE_MIKROBUS.pdc" \
+	-file "${project_dir}/constraint/io/P9.pdc" \
 	-file "${project_dir}/constraint/io/ICICLE_SDIO.pdc" \
 	-file "${project_dir}/constraint/io/ICICLE_USB.pdc" \
 	-file "${project_dir}/constraint/io/ICICLE.pdc" \
